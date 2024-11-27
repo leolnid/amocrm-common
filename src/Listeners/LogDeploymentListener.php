38020f659a226e145013bee9bf94fdfe02089ca9
Леонид Дюков <l.dyukov@makeroi.ru>
2024-11-27 23:30:27 +0300

    Изменение логгирования

--- src/Listeners/LogDeploymentListener.php
+++ src/Listeners/LogDeploymentListener.php
@@ -10,7 +10,7 @@ class LogDeploymentListener
 
     public function handle($event): void
     {
-        logger()->channel('telegram')->notice('Завершили обновление приложения');
+        \App\Services\TelegramLogger::info('Завершили обновление приложения');
         logger()->notice('Завершили обновление приложения');
     }
 }
